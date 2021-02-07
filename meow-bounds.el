e84684f03e7912f29b70a02175bda34d1573e51d
Moesasji <miyamoto.31b@gmail.com>
2021-02-07 16:42:37 +0000

    add copyright clause to meow-bounds.el

--- meow-bounds.el
+++ meow-bounds.el
@@ -1,5 +1,22 @@
 ;;; meow-bounds.el --- Calculate bounds in Meow  -*- lexical-binding: t -*-
 
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
 (require 'cl-lib)
 (require 'subr-x)
 (require 'dash)
