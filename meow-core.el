3e58697695327d1ecf2a210af645e8f2db845c32
kekeimiku <kekelanact@gmail.com>
2021-09-08 23:32:06 +0800

    fix emacs28 warning (#69)
    * fix emacs28 warning
    
    * fix emacs28 warning

--- meow-core.el
+++ meow-core.el
@@ -36,35 +36,35 @@
 ;;;###autoload
 (define-minor-mode meow-insert-mode
   "Meow Insert state."
-  nil
-  " [I]"
-  meow-insert-state-keymap
+  :init-value nil
+  :lighter " [I]"
+  :keymap meow-insert-state-keymap
   (meow--insert-init))
 
 ;;;###autoload
 (define-minor-mode meow-normal-mode
   "Meow Normal state."
-  nil
-  " [N]"
-  meow-normal-state-keymap
+  :init-value nil
+  :lighter " [N]"
+  :keymap meow-normal-state-keymap
   (meow--normal-init))
 
 ;;;###autoload
 (define-minor-mode meow-keypad-mode
   "Meow keypad state."
-  nil
-  " [K]"
+  :init-value nil
+  :lighter " [K]"
   ;; use overriding-local-map for highest keymap priority
   ;; so KEYPAD won't be affected by overlays' keymap
-  meow-keypad-state-keymap
+  :keymap meow-keypad-state-keymap
   (meow--keypad-init))
 
 ;;;###autoload
 (define-minor-mode meow-motion-mode
   "Meow motion state."
-  nil
-  " [M]"
-  meow-motion-state-keymap
+  :init-value nil
+  :lighter " [M]"
+  :keymap meow-motion-state-keymap
   (meow--motion-init))
 
 ;;;###autoload
@@ -72,9 +72,9 @@
   "Meow minor mode.
 
 This minor mode is used by meow-global-mode, should not be enabled directly."
-  nil
-  nil
-  meow-keymap
+  :init-value nil
+  :global nil
+  :keymap meow-keymap
   (if meow-mode
       (meow--enable)
     (meow--disable)))
