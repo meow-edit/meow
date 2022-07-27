meow-edit/meow

commit 6741f4dc51bc44d8fe0f01bf1aff48ae6ec237f5

remove beacon cursors when beacon mode is disabled
Solves #290. Now when beacon mode is disabled, we call
meow--beacon-remove-overlays do disable the fake cursor overlays.
Note the secondary selection remains active. When beacon mode is
resumed, the cursors are preserved. Because all states are disabled
from meow--disable, this solves the case where meow-mode is called
directly.

meow-core.el
@@ -90,7 +90,8 @@
         (meow--cancel-selection)
         (hl-line-mode -1))
     (when meow--beacon-backup-hl-line
-      (hl-line-mode 1))))
+      (hl-line-mode 1))
+    (meow--beacon-remove-overlays)))
 
 ;;;###autoload
 (define-minor-mode meow-mode

commit 7471762ec043fa85a91398b2b5b05859da544200

fix meow-beacon bug
bug introduced in 6741f4dc51bc44d8fe0f01bf1aff48ae6ec237f5.
fixes #291.

meow-command.el
@@ -1591,14 +1591,17 @@ This command is a replacement for build-in `kmacro-end-macro'."
 ;;; GRAB SELECTION
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun meow--cancel-second-selection ()
+  (delete-overlay mouse-secondary-overlay)
+  (setq mouse-secondary-start (make-marker))
+  (move-marker mouse-secondary-start (point)))
+
 (defun meow-grab ()
   "Create secondary selection or a marker if no region available."
   (interactive)
   (if (region-active-p)
       (secondary-selection-from-region)
-    (delete-overlay mouse-secondary-overlay)
-    (setq mouse-secondary-start (make-marker))
-    (move-marker mouse-secondary-start (point)))
+    (meow--cancel-second-selection))
   (meow--cancel-selection))
 
 (defun meow-pop-grab ()

meow-core.el
@@ -90,8 +90,7 @@
         (meow--cancel-selection)
         (hl-line-mode -1))
     (when meow--beacon-backup-hl-line
-      (hl-line-mode 1))
-    (meow--beacon-remove-overlays)))
+      (hl-line-mode 1))))
 
 ;;;###autoload
 (define-minor-mode meow-mode
@@ -156,7 +155,10 @@ there's no chance for meow to call an init function."
 
 (defun meow--disable ()
   "Disable Meow."
-  (mapc (lambda (state-mode) (funcall (cdr state-mode) -1)) meow-state-mode-alist))
+  (mapc (lambda (state-mode) (funcall (cdr state-mode) -1)) meow-state-mode-alist)
+  (meow--beacon-remove-overlays)
+  (when (secondary-selection-exist-p)
+    (meow--cancel-second-selection)))
 
 (defun meow--global-enable ()
   "Enable meow globally."

meow-util.el
@@ -204,7 +204,8 @@ Looks up the state in meow-replace-state-name-list"
 (defun meow--exit-keypad-state ()
   "Exit keypad state."
   (meow-keypad-mode -1)
-  (when (eq 'beacon meow--keypad-previous-state)
+  (when (and (eq 'beacon meow--keypad-previous-state)
+             meow--current-state)
     (meow--beacon-apply-command meow--keypad-this-command))
   (when meow--keypad-previous-state
     (meow--switch-state meow--keypad-previous-state)))