c9229836f9fe486e89d654f413d32cbb4fdf04f0
tprost <tprost@users.noreply.github.com>
2024-10-04 23:48:45 -0400

    nuke meow, use Evil

--- helix.el
+++ helix.el
@@ -1,16 +1,26 @@
-;;; helix.el --- Description of the package -*- lexical-binding: t; -*-
+;;; helix.el --- Sort of emulate Helix I guess -*- lexical-binding: t; -*-
 
-;; Package-Requires: ((emacs "27.1") (evil "1.14.0"))
-;; Keywords: helix, evil
-;; Version: 0.1.0
 ;; Author: <tprost@users.noreply.github.com>
-;; URL: https://github.com/yourname/my-package
+;; Keywords: convenience, helix, modal-editing
+;; Package-Requires: ((emacs "27.1") (evil "1.14.0"))
+;; Version: 0.1.0
+;;
+;; This file is not part of GNU Emacs.
 
-;;; Commentary:
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
 
-;; This package provides some Evil mode features.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;;; Code:
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 (require 'helix-evil)
 (require 'helix-commands)
